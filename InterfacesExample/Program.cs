0906de80a63e9124a2935b0332d06fa91ed1c5e2
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-04-30 14:41:52 +0300

    30.04.2021 - keep learning

---
+++ InterfacesExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace InterfacesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new EmployeeManager());
+            projectManager.Add(new InternManager());
+            projectManager.Add(new CustomerManager());
+
+            Console.WriteLine("--------------------------------------");
+
+            projectManager.Update(new EmployeeManager());
+            projectManager.Update(new InternManager());
+            projectManager.Update(new CustomerManager());
+
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi.");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("İşçi Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("İşçi Güncellendi.");
+        }
+    }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi.");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+        public void Update(IPersonManager personManager)
+        {
+            personManager.Update();
+        }
+    }
+}
---
+++ LinqDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId = 1, CategoryName = "Bilgisayar"},
+                new Category{CategoryId = 2, CategoryName = "Telefon"}
+            };
+
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId=1, CategoryId=1, ProductName="Lenovo Dizüstü Bilgisayar", UnitPrice=7500, UnitsInStock=55},
+                new Product{ProductId=2, CategoryId=1, ProductName="Huawei Dizüstü Bilgisayar", UnitPrice=7100, UnitsInStock=32},
+                new Product{ProductId=3, CategoryId=2, ProductName="Samsung Akıllı Telefon", UnitPrice=4900, UnitsInStock=140},
+                new Product{ProductId=4, CategoryId=2, ProductName="LG Akıllı Telefon", UnitPrice=4350, UnitsInStock=700}
+            };
+
+            
+            GetProducts(products);
+
+            GetProductswithLinq(products);
+
+        }
+
+        //ürünleri, kategorileri nasıl listeleriz? yöntem-1 => foreach döngüsü
+        static List<Product> GetProducts(List<Product> products)
+        {
+            List<Product> filteredProduct = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000)
+                {
+                    filteredProduct.Add(product);
+                }
+            }
+            return filteredProduct;
+        }
+
+        //ürünleri, kategorileri nasıl listeleriz? yöntem-2 => Linq ile listeleme
+        static List<Product> GetProductswithLinq(List<Product> products)
+        {
+            return products.Where(p=>p.UnitPrice>5000).ToList();
+            
+        }
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
